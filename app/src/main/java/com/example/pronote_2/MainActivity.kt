c5a94245735920eddf3731847145c23626eca4df
Scared Cat <ricardooliveiraprivate@hotmail.com>
2025-12-01 11:39:19 +0100

    refactor: Cleaning code

--- app/src/main/java/com/example/pronote_2/MainActivity.kt
+++ app/src/main/java/com/example/pronote_2/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.pronote_2
 
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,29 +14,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pronote_2.ui.theme.ProNote_2Theme
 
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
-import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,13 +30,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProNote_2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
-                    CenterAlignedTopAppBarExample()
+                Scaffold(topBar = {
+                    ProNote2TopAppBar(name = "ProNote 2")
                 }) { innerPadding ->
-                    Greeting(
-                        name = "ProNote 2",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        Greeting(
+                            name = "ProNote 2",
+                        )}
                 }
             }
         }
@@ -60,8 +50,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    Text(text = name, textAlign = TextAlign.Center)
+    Text(text = name)
 }
 
 @Preview(showBackground = true)
@@ -74,34 +63,27 @@ fun GreetingPreview() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CenterAlignedTopAppBarExample() {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(
-                        "Centered Top App Bar",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                },
-                actions = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                },
+fun ProNote2TopAppBar(name: String) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(
+                name,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
         },
-        ) {
-            innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) { }
-    }
+        actions = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+    )
 }
 
