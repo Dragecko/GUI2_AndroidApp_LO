69021fe44b30094fb021f1a66d67ee9a1fa13d8d
Dragecko <dragecko@etik.com>
2026-01-03 22:08:39 +0100

    feat: adding the page for adding grades

---
+++ app/src/main/java/com/example/pronote_2/ui/AddNoteActivity.kt
@@ -0,0 +1,25 @@
+package com.example.pronote_2.ui.theme
+import android.app.DatePickerDialog
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.*
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.pronote_2.R
+import java.util.Calendar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddNoteScreen() {
+
+}
